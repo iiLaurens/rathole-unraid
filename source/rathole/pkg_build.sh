6c2ffb4e3d7b1dd79c8a78039c0926d6346274b8
digi-deity <git@laurens.xyz>
2026-05-14 16:15:46 +0200

    First release

--- source/rathole/pkg_build.sh
+++ source/rathole/pkg_build.sh
@@ -10,7 +10,7 @@ mkdir -p $tmpdir
 cd $DIR
 cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" \) ) $tmpdir/
 cd $tmpdir
-find . -type f \( -iname '*.sh' -o -iname '*.page' -o -iname '*.plg' \) -exec sed -i 's/\r//g' {} +
+find . -type f \( -iname '*.sh' -o -iname '*.page' -o -iname '*.plg' -o -iname 'rc.*' \) -exec sed -i 's/\r//g' {} +
 chmod -R +x ./
 tar cfJCo ${archive}/${plugin}-${version}-x86_64-1.txz $tmpdir . --owner=0 --group=0 --mode="a=r,u+w,a+X"
 rm -rf $tmpdir
